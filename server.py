7b3d5f9b8dd4510aada4ec93c2130f9f4a826072
Denis Shihalev <shihalev.denis@gmail.com>
2020-08-22 16:28:15 +0300

    A simple working one-page with templates

--- server.py
+++ server.py
@@ -1,9 +1,16 @@
 import asyncio
 from aiohttp import web
-from routes import setup_routes
+from routes import setup_routes, setup_static_routes
+import aiohttp_jinja2
+import jinja2
+from settings import BASE_DIR
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
     app = web.Application()
+    aiohttp_jinja2.setup(app,
+                         loader=jinja2.FileSystemLoader(str(BASE_DIR / 'templates')))
+    print(str(BASE_DIR / 'templates'))
     setup_routes(app)
+    setup_static_routes(app)
     web.run_app(app)
--- views.py
+++ views.py
@@ -1,5 +1,7 @@
 from aiohttp import web
+import aiohttp_jinja2
 
 
+@aiohttp_jinja2.template('index.html')
 async def index(request):
-    return web.Response(text="Hello Aiohttp!")
+    return {"variable": 5}
