deba3fbc804fd6fb67ce3953043df9549fc9bbef
Denis Shihalev <shihalev.denis@gmail.com>
2020-09-08 22:00:59 +0300

    Full board and mouse click checking

--- static/board.js
+++ static/board.js
@@ -2,34 +2,42 @@ var example = document.getElementById("example"),
 ctx = example.getContext('2d');
 example.width  = 640;
 example.height = 480;
-let start_x = 20
-let start_y = 20
-let size = 32
+
+let size = 25
 let frame_margin_1 = 2
 let frame_margin_2 = 5
 let square_count_x = 4
 let square_count_y = 4
 
-let frame_1_start_x = start_x - frame_margin_1
-let frame_2_start_x = start_x - frame_margin_2
-let frame_1_start_y = start_y - frame_margin_1
-let frame_2_start_y = start_y - frame_margin_2
-let board_size_x = size*square_count_x
-let board_size_y = size*square_count_y
-let frame_1_size_x = board_size_x + 2*frame_margin_1
-let frame_1_size_y = board_size_y + 2*frame_margin_1
-let frame_2_size_x = board_size_x + 2*frame_margin_2
-let frame_2_size_y = board_size_y + 2*frame_margin_2
+function draw_square(start_x, start_y, size, frame_margin_1, frame_margin_2, square_count_x, square_count_y)
+{
+    let frame_1_start_x = start_x - frame_margin_1
+    let frame_2_start_x = start_x - frame_margin_2
+    let frame_1_start_y = start_y - frame_margin_1
+    let frame_2_start_y = start_y - frame_margin_2
+    let board_size_x = size*square_count_x
+    let board_size_y = size*square_count_y
+    let frame_1_size_x = board_size_x + 2*frame_margin_1
+    let frame_1_size_y = board_size_y + 2*frame_margin_1
+    let frame_2_size_x = board_size_x + 2*frame_margin_2
+    let frame_2_size_y = board_size_y + 2*frame_margin_2
 
 
-ctx.strokeRect(frame_1_start_x, frame_1_start_y, frame_1_size_x, frame_1_size_y);
-ctx.strokeRect(frame_2_start_x, frame_2_start_y, frame_2_size_x, frame_2_size_y);
-ctx.fillStyle = '#AF5200'; // меняем цвет клеток
+    ctx.strokeRect(frame_1_start_x, frame_1_start_y, frame_1_size_x, frame_1_size_y);
+    ctx.strokeRect(frame_2_start_x, frame_2_start_y, frame_2_size_x, frame_2_size_y);
+    ctx.fillStyle = '#AF5200'; // меняем цвет клеток
 
-for (i = 0; i < square_count_x; i += 1)
-    for (j = 0; j < square_count_y; j += 1) {
-        ctx.fillRect(start_x + i * size, start_y + j * size, size-1, size-1);
-    }
+    for (i = 0; i < square_count_x; i += 1)
+        for (j = 0; j < square_count_y; j += 1) {
+            ctx.fillRect(start_x + i * size, start_y + j * size, size-1, size-1);
+        }
+}
+
+draw_square(20, 20, size, frame_margin_1, frame_margin_2, square_count_x, square_count_y)
+draw_square(180, 20, size, frame_margin_1, frame_margin_2, square_count_x, square_count_y)
+draw_square(340, 20, size, frame_margin_1, frame_margin_2, square_count_x, square_count_y)
+draw_square(500, 20, size, frame_margin_1, frame_margin_2, square_count_x, square_count_y)
+
 
 pic = new Image();              // "Создаём" изображение
 pic.src    = 'http://habrahabr.ru/i/nocopypast.png';  // Источник изображения, позаимствовано на хабре
@@ -38,3 +46,16 @@ pic.onload = function() {    // Событие onLoad, ждём момента �
    //ctx.drawImage(pic, 0, 0, 300, 150);
    //ctx.drawImage(pic, 25, 42, 85, 55, 0, 0, 170, 110);
 }
+
+function windowToCanvas(canvas, x, y) {
+    var bbox = canvas.getBoundingClientRect();
+    return { x: x - bbox.left * (canvas.width / bbox.width),
+        y: y - bbox.top * (canvas.height / bbox.height)
+    };
+}
+
+example.addEventListener('mousedown', function (e) {
+    res = windowToCanvas(example, e.clientX, e.clientY);
+    document.getElementById("debug").value += (res.x+ "  ");
+
+});
