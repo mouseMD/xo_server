91c859e19246ae5554d6e1be12ba897d3dfc633d
Denis Shihalev <shihalev.denis@gmail.com>
2020-09-10 21:30:32 +0300

    Getting cell indexes

--- static/board.js
+++ static/board.js
@@ -9,35 +9,63 @@ let frame_margin_2 = 5
 let square_count_x = 4
 let square_count_y = 4
 
-function draw_square(start_x, start_y, size, frame_margin_1, frame_margin_2, square_count_x, square_count_y)
-{
-    let frame_1_start_x = start_x - frame_margin_1
-    let frame_2_start_x = start_x - frame_margin_2
-    let frame_1_start_y = start_y - frame_margin_1
-    let frame_2_start_y = start_y - frame_margin_2
-    let board_size_x = size*square_count_x
-    let board_size_y = size*square_count_y
-    let frame_1_size_x = board_size_x + 2*frame_margin_1
-    let frame_1_size_y = board_size_y + 2*frame_margin_1
-    let frame_2_size_x = board_size_x + 2*frame_margin_2
-    let frame_2_size_y = board_size_y + 2*frame_margin_2
+class BigSquare{
+constructor(start_x, start_y, size, frame_margin_1, frame_margin_2, square_count_x, square_count_y, color) {
+    this.start_x = start_x;
+    this.start_y = start_y;
+    this.size = size;
+    this.frame_margin_1 = frame_margin_1;
+    this.frame_margin_2 = frame_margin_2;
+    this.square_count_x = square_count_x;
+    this.square_count_y = square_count_y;
+    this.color = color;
 
+    this.frame_1_start_x = this.start_x - this.frame_margin_1;
+    this.frame_2_start_x = this.start_x - this.frame_margin_2;
+    this.frame_1_start_y = this.start_y - this.frame_margin_1;
+    this.frame_2_start_y = this.start_y - this.frame_margin_2;
+    this.board_size_x = this.size * this.square_count_x;
+    this.board_size_y = this.size * this.square_count_y;
+    this.frame_1_size_x = this.board_size_x + 2 * this.frame_margin_1;
+    this.frame_1_size_y = this.board_size_y + 2 * this.frame_margin_1;
+    this.frame_2_size_x = this.board_size_x + 2 * this.frame_margin_2;
+    this.frame_2_size_y = this.board_size_y + 2 * this.frame_margin_2;
+}
 
-    ctx.strokeRect(frame_1_start_x, frame_1_start_y, frame_1_size_x, frame_1_size_y);
-    ctx.strokeRect(frame_2_start_x, frame_2_start_y, frame_2_size_x, frame_2_size_y);
-    ctx.fillStyle = '#AF5200'; // меняем цвет клеток
+draw() {
+    ctx.strokeRect(this.frame_1_start_x, this.frame_1_start_y, this.frame_1_size_x, this.frame_1_size_y);
+    ctx.strokeRect(this.frame_2_start_x, this.frame_2_start_y, this.frame_2_size_x, this.frame_2_size_y);
+    ctx.fillStyle = this.color; // меняем цвет клеток
 
-    for (i = 0; i < square_count_x; i += 1)
-        for (j = 0; j < square_count_y; j += 1) {
-            ctx.fillRect(start_x + i * size, start_y + j * size, size-1, size-1);
+    for (let i = 0; i < this.square_count_x; i += 1)
+        for (let j = 0; j < this.square_count_y; j += 1) {
+            ctx.fillRect(this.start_x + i * this.size, this.start_y + j * this.size, this.size-1, this.size-1);
         }
 }
 
-draw_square(20, 20, size, frame_margin_1, frame_margin_2, square_count_x, square_count_y)
-draw_square(180, 20, size, frame_margin_1, frame_margin_2, square_count_x, square_count_y)
-draw_square(340, 20, size, frame_margin_1, frame_margin_2, square_count_x, square_count_y)
-draw_square(500, 20, size, frame_margin_1, frame_margin_2, square_count_x, square_count_y)
+contains(xPos, yPos) {
+    return (this.start_x <= xPos) && (this.start_x + this.board_size_x > xPos) &&
+            (this.start_y <= yPos) && (this.start_y + this.board_size_y > yPos);
+}
 
+
+getCoords(xPos, yPos) {
+    let x_index = Math.floor((xPos - this.start_x) / this.size);
+    let y_index = Math.floor((yPos - this.start_y) / this.size);
+    return {x: x_index, y: y_index}
+}
+
+}
+
+let square1 = new BigSquare(20, 20, size, frame_margin_1, frame_margin_2, square_count_x, square_count_y, '#AF5200');
+let square2 = new BigSquare(180, 20, size, frame_margin_1, frame_margin_2, square_count_x, square_count_y, '#AF5200');
+let square3 = new BigSquare(340, 20, size, frame_margin_1, frame_margin_2, square_count_x, square_count_y, '#AF5200');
+let square4 = new BigSquare(500, 20, size, frame_margin_1, frame_margin_2, square_count_x, square_count_y, '#AF5200');
+
+square1.draw();
+square2.draw();
+square3.draw();
+square4.draw();
 
 pic = new Image();              // "Создаём" изображение
 pic.src    = 'http://habrahabr.ru/i/nocopypast.png';  // Источник изображения, позаимствовано на хабре
@@ -55,7 +83,30 @@ function windowToCanvas(canvas, x, y) {
 }
 
 example.addEventListener('mousedown', function (e) {
-    res = windowToCanvas(example, e.clientX, e.clientY);
-    document.getElementById("debug").value += (res.x+ "  ");
+    let res = windowToCanvas(example, e.clientX, e.clientY);
+    document.getElementById("debug").value += (" -"+res.x+ "--"+res.y+"- ");
+    if (square1.contains(res.x, res.y)){
+        let indexes = square1.getCoords(res.x, res.y)
+        document.getElementById("debug").value += (" *0**"+indexes.x+ "**"+indexes.y+"* ");
+    }
+    else if (square2.contains(res.x, res.y)){
+        let indexes = square2.getCoords(res.x, res.y)
+        document.getElementById("debug").value += (" *1**"+indexes.x+ "**"+indexes.y+"* ");
+    }
+        else if (square3.contains(res.x, res.y)){
+        let indexes = square3.getCoords(res.x, res.y)
+        document.getElementById("debug").value += (" *2**"+indexes.x+ "**"+indexes.y+"* ");
+    }
+        else if (square4.contains(res.x, res.y)){
+        let indexes = square4.getCoords(res.x, res.y)
+        document.getElementById("debug").value += (" *3**"+indexes.x+ "**"+indexes.y+"* ");
+    }
+    else {
+        document.getElementById("debug").value += (" missed!");
+    }
+
+
+
+
 
 });
