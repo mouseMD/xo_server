e7f144c57aaae15ec2736cb5a4586bf1a4059daf
Denis Shihalev <shihalev.denis@gmail.com>
2020-12-15 12:06:39 +0300

    Decorator for creating temporary user id

--- views.py
+++ views.py
@@ -1,30 +1,33 @@
-from aiohttp import web, WSMsgType
+from aiohttp import web
 import aiohttp_jinja2
-from aiohttp_security import is_anonymous, remember, forget, authorized_userid
+from aiohttp_security import is_anonymous, remember, authorized_userid
 from auth import get_new_anonymous_user_id
 
 
+# decorator to autocreate temporary user ids for not autheticated usera
+def auto_new_user(handler):
+    async def wrap_handler(request):
+        is_logged = not await is_anonymous(request)
+        if is_logged:
+            return await handler(request)
+        else:
+            user_id = "User#" + str(get_new_anonymous_user_id())
+            redirect_response = web.HTTPFound(request.rel_url)
+            await remember(request, redirect_response, user_id)
+            raise redirect_response
+    return wrap_handler
+
+
 @aiohttp_jinja2.template('index.html')
+@auto_new_user
 async def index(request):
-    is_logged = not await is_anonymous(request)
-    if is_logged:
-        user_id = await authorized_userid(request)
-        return {'variable': user_id}
-    else:
-        user_id = "User#" + str(get_new_anonymous_user_id())
-        redirect_response = web.HTTPFound('/')
-        await remember(request, redirect_response, user_id)
-        raise redirect_response
+    user_id = await authorized_userid(request)
+    return {'variable': user_id}
 
 
 @aiohttp_jinja2.template('wait_game.html')
+@auto_new_user
 async def wait_game(request):
-    is_logged = not await is_anonymous(request)
-    if is_logged:
-        user_id = await authorized_userid(request)
-        return {'user': user_id}
-    else:
-        user_id = "User#" + str(get_new_anonymous_user_id())
-        redirect_response = web.HTTPFound('/wait_game')
-        await remember(request, redirect_response, user_id)
-        raise redirect_response
+    user_id = await authorized_userid(request)
+    return {'user': user_id}
+
