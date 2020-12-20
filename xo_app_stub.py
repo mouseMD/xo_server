c0a0ef60779dfdf37e40df4286151ca36176367c
Denis Shihalev <shihalev.denis@gmail.com>
2020-12-20 12:31:51 +0300

    Modified xo_app_stub interface, added docstrings and typehints.

--- xo_app_stub.py
+++ xo_app_stub.py
@@ -1,54 +1,113 @@
 import xo_app
+from typing import Sequence
 
 ga = xo_app.GameApplication()
 
 
-def create_new_game():
+class InvalidCoordsException(Exception):
+    def __init__(self):
+        super().__init__()
+
+
+class InvalidPlayerException(Exception):
+    def __init__(self):
+        super().__init__()
+
+
+def create_new_game() -> int:
+    """
+    Allocate memory for a new game.
+
+    Returns unique game id.
+    """
     return ga.create_new_game()
 
 
-def set_new_move(game_index, player, coords):
+def set_new_move(game_index: int, player: str, coords: Sequence) -> None:
+    """
+    Make a new move in game.
+
+    coords must be sequence of 3 valid numbers.
+    player must be one of options: 'first' or 'second'.
+    """
+    if len(coords) != 3:
+        raise InvalidCoordsException()
+    try:
+        for item in coords:
+            if item < 0 or item > 3:
+                raise InvalidCoordsException()
+    except TypeError:
+        raise InvalidCoordsException()
     c = xo_app.Coordinates(*coords)
-    p = xo_app.Player.X_Player if player == 'first' else xo_app.Player.O_Player
+
+    if player == 'first':
+        p = xo_app.Player.X_Player
+    elif player == 'second':
+        p = xo_app.Player.O_Player
+    else:
+        raise InvalidPlayerException()
     ga.set_new_move(game_index, p, c)
 
 
-def release_game(game_index):
+def release_game(game_index) -> None:
+    """
+    Delete game data.
+    """
     ga.release_game(game_index)
 
 
-def finalize():
+def finalize() -> None:
+    """
+    Free all resources used by application.
+    """
     ga.finalize()
 
 
-def finished(game_index):
+def finished(game_index) -> bool:
+    """
+    Check if the game is in its final state.
+    """
     return ga.finished(game_index)
 
 
-def started(game_index):
-    return ga.started(game_index)
+def result(game_index) -> str:
+    """
+    Return result of the game.
+    """
+    results = {0: 'first_win',
+               1: 'second_win',
+               2: 'draw',
+               3: 'none'}
+    int_result = ga.result(game_index)
+    return results[int_result]
 
 
-def result(game_index):
-    return ga.result(game_index)
-
-
-def get_moves(game_index):
+def get_moves(game_index) -> str:
+    """
+    Return all moves from the game.
+    """
     return ga.get_moves(game_index)
 
 
-def get_win_coords(game_index):
+def get_win_coords(game_index) -> str:
+    """
+    Return squares of win combination.
+    """
     return ga.get_win_coords(game_index)
 
 
-def exist(game_index):
-    return ga.exist(game_index)
-
-
-def get_board(game_index):
+def get_board(game_index) -> str:
+    """
+    Return full board shapshot.
+    """
     return ga.get_board(game_index)
 
 
-def get_player_to_move(game_index):
+def get_player_to_move(game_index) -> str:
+    """
+    Return player to move now.
+
+    Return values are 'first' or 'second'.
+    """
     p = ga.get_player_to_move(game_index)
     return 'first' if p == xo_app.Player.X_Player else 'second'
