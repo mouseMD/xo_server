3a7e5d1f2a3da7d031b4a8f3e0c1ed05fc906a0c
Denis Shihalev <shihalev.denis@gmail.com>
2021-05-05 21:23:38 +0300

    Add user model(only description)

---
+++ models.py
@@ -0,0 +1,18 @@
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import Integer, String, Boolean, Column
+
+Base = declarative_base()
+
+
+class User(Base):
+    """The User model."""
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True, autoincrement=True),
+    created_at = Column(Integer),
+    last_seen_at = Column(Integer),
+    login = Column(String(32), nullable=False, unique=True),
+    password_hash = Column(String(256), nullable=False),
+    online = Column(Boolean, default=False),
+    deleted = Column(Boolean, default=False)
+
+    __mapper_args__ = {"eager_defaults": True}
