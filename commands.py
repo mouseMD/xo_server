1c923dab6a41637b6888c6b96d4e938497d8829d
Denis Shihalev <shihalev.denis@gmail.com>
2021-05-06 16:57:47 +0300

    Implement data method for commands

--- commands.py
+++ commands.py
@@ -101,9 +101,8 @@ class InCommand(Command):
 
 class OutCommand(Command):
     """
-    Input commands (from user to server)
+    Output commands (from server to user)
     """
-
     def __init__(self, user_id):
         super().__init__(user_id)
         self.direction = "out"
@@ -117,11 +116,28 @@ class WaitingCommand(OutCommand):
     def __init__(self, user_id, **parameters):
         super().__init__(user_id)
 
+    def data(self):
+        return {
+            'version': 'v1',
+            'command': 'waiting',
+            'parameters': {}
+        }
+
 
 class ErrorCommand(OutCommand):
     def __init__(self, user_id, **parameters):
         super().__init__(user_id)
         self.message = parameters["msg"]
+
+    def data(self):
+        params = {
+            'msg': self.message
+        }
+        return {
+            'version': 'v1',
+            'command': 'error',
+            'parameters': params
+        }
 
 
 class StartedCommand(OutCommand):
@@ -129,6 +145,17 @@ class StartedCommand(OutCommand):
         super().__init__(user_id)
         self.opponent = parameters["opp_id"]
         self.ptype = parameters["ptype"]
+
+    def data(self):
+        params = {
+            'opp_id': self.opponent,
+            'ptype': self.ptype
+        }
+        return {
+            'version': 'v1',
+            'command': 'started',
+            'parameters': params
+        }
 
 
 class UpdateStateCommand(OutCommand):
@@ -138,10 +165,29 @@ class UpdateStateCommand(OutCommand):
         self.p_t_m = parameters["player_to_move"]
         self.last_move = parameters["last_move"]
 
+    def data(self):
+        params = {
+            'board': self.board,
+            'player_to_move': self.p_t_m,
+            'last_move': self.last_move
+        }
+        return {
+            'version': 'v1',
+            'command': 'update_state',
+            'parameters': params
+        }
+
 
 class OfferedCommand(OutCommand):
     def __init__(self, user_id, **parameters):
         super().__init__(user_id)
+
+    def data(self):
+        return {
+            'version': 'v1',
+            'command': 'offered',
+            'parameters': {}
+        }
 
 
 class GameOverCommand(OutCommand):
@@ -151,17 +197,47 @@ class GameOverCommand(OutCommand):
         self.win_pos = parameters["win_pos"]
         self.cause = parameters["cause"]
 
+    def data(self):
+        params = {
+            'result': self.result,
+            'win_pos': self.win_pos,
+            'cause': self.cause
+        }
+        return {
+            'version': 'v1',
+            'command': 'game_over',
+            'parameters': params
+        }
+
 
 class ReadyCommand(InCommand):
     def __init__(self, user_id, **parameters):
         super().__init__(user_id)
         self.type = parameters["type"]
-        self.opponent = parameters["oppponent"]
+        self.opponent = parameters["opponent"]
+
+    def data(self):
+        params = {
+            'type': self.type,
+            'opponent': self.opponent,
+        }
+        return {
+            'version': 'v1',
+            'command': 'ready',
+            'parameters': params
+        }
 
 
 class ResignCommand(InCommand):
     def __init__(self, user_id, **parameters):
         super().__init__(user_id)
+
+    def data(self):
+        return {
+            'version': 'v1',
+            'command': 'resign',
+            'parameters': {}
+        }
 
 
 class MoveCommand(InCommand):
@@ -171,15 +247,41 @@ class MoveCommand(InCommand):
         self.vertical = parameters["vertical"]
         self.horizontal = parameters["horizontal"]
 
+    def data(self):
+        params = {
+            'square': self.square,
+            'vertical': self.vertical,
+            'horizontal': self.horizontal
+        }
+        return {
+            'version': 'v1',
+            'command': 'move',
+            'parameters': params
+        }
+
 
 class OfferCommand(InCommand):
     def __init__(self, user_id, **parameters):
         super().__init__(user_id)
 
+    def data(self):
+        return {
+            'version': 'v1',
+            'command': 'offer',
+            'parameters': {}
+        }
+
 
 class AcceptCommand(InCommand):
     def __init__(self, user_id, **parameters):
         super().__init__(user_id)
+
+    def data(self):
+        return {
+            'version': 'v1',
+            'command': 'accept',
+            'parameters': {}
+        }
 
 
 class CommandFactory(ABC):
