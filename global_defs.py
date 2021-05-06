b0fb999fa4a6a92029160d4165ea1db9ed1ab252
Denis Shihalev <shihalev.denis@gmail.com>
2021-05-06 16:02:26 +0300

    Add socket registry

--- global_defs.py
+++ global_defs.py
@@ -1,3 +1,19 @@
 from players import Playground, Matcher
 global_playground = Playground(Matcher())
 global_sockets = {}
+
+
+class SocketRegistry:
+    """
+    Registry for all user sockets
+    """
+    sockets = {}
+
+    def get_socket(self, user_id):
+        return self.sockets[user_id]
+
+    def add_socket(self, user_id, ws):
+        self.sockets[user_id] = ws
+
+    def remove_socket(self, user_id):
+        self.sockets.pop(user_id, None)
