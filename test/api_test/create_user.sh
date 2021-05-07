0e6a60ceafe14f747f7bf7140f91fe425db5a0d0
Denis Shihalev <shihalev.denis@gmail.com>
2021-05-07 20:00:56 +0300

    Add parameters to create_user test

--- test/api_test/create_user.sh
+++ test/api_test/create_user.sh
@@ -1,1 +1,5 @@
-curl -X POST -F 'login=user1' -F 'password=user1' localhost:8080/users
+if [ $# -ne 2 ]; then
+  echo "illegal number of parameters: $#"
+else
+  curl -X POST -F "login=$1" -F "password=$2" localhost:8080/users
+fi
