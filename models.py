dfed697e89e21d3d2d41ff95f35c57d145b06a75
Denis Shihalev <shihalev.denis@gmail.com>
2021-05-07 18:56:56 +0300

    Fix bug in User class

--- models.py
+++ models.py
@@ -13,12 +13,12 @@ class UserException(Exception):
 class User(Base):
     """The User model."""
     __tablename__ = 'users'
-    id = Column(Integer, primary_key=True, autoincrement=True),
-    created_at = Column(Integer, default=current_timestamp),
-    last_seen_at = Column(Integer, default=current_timestamp, onupdate=current_timestamp),
-    login = Column(String(32), nullable=False, unique=True),
-    password_hash = Column(String(256), nullable=False),
-    online = Column(Boolean, default=True),
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    created_at = Column(Integer, default=current_timestamp)
+    last_seen_at = Column(Integer, default=current_timestamp, onupdate=current_timestamp)
+    login = Column(String(32), nullable=False, unique=True)
+    password_hash = Column(String(256), nullable=False)
+    online = Column(Boolean, default=True)
     deleted = Column(Boolean, default=False)
 
     __mapper_args__ = {"eager_defaults": True}
