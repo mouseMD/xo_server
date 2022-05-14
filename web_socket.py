e317965b5101365c6b4b68d1f823f5dff19821f7
Denis Shihalev <shihalev.denis@gmail.com>
2022-05-14 12:23:57 +0300

    Refactor web_socket.py

--- web_socket.py
+++ web_socket.py
@@ -12,41 +12,74 @@ async def websocket_handler(request):
     ws = web.WebSocketResponse()
     try:
         await ws.prepare(request)
-    except web.HTTPException as e:
+    except web.HTTPException:
         logging.info('Failed to open WebSocket')
     else:
-        user_id = await authorized_userid(request)
-        logging.info("websocket connection opened with user_id {}".format(user_id))
-        # if already connected, not permit connection
-        try:
-            global_playground.register(user_id)
-        except AlreadyRegistered:
-            logging.info("Deliberately closed connection with already connected user_id {}!".format(user_id))
-            await send_command(ErrorCommand(user_id, msg=f'User id {user_id} already in use'))
-            await ws.close()
-        else:
-            registry.add_socket(user_id, ws)
-            async for msg in ws:
-                if msg.type == WSMsgType.TEXT:
-                    await handle_command(json.loads(msg.data), user_id)
-                elif msg.type == WSMsgType.ERROR:
-                    logging.info('connection closed with exception {} with user_id {}'.format(ws.exception(), user_id))
-                    await handle_error(user_id)
-                elif msg.type == WSMsgType.BINARY:
-                    logging.info('Received BINARY type message')
-                elif msg.type == WSMsgType.CLOSE:
-                    logging.info('Received CLOSE type message')
-                elif msg.type == WSMsgType.CLOSED:
-                    logging.info('Received CLOSED type message')
-                elif msg.type == WSMsgType.CLOSING:
-                    logging.info('Received CLOSING type message')
-                elif msg.type == WSMsgType.CONTINUATION:
-                    logging.info('Received CONTINUATION type message')
-                elif msg.type == WSMsgType.PING:
-                    logging.info('Received PING type message')
-                elif msg.type == WSMsgType.PONG:
-                    logging.info('Received PONG type message')
-
-            logging.info('websocket connection closed with user_id {}'.format(user_id))
-            await handle_error(user_id)
+        await process_connection(ws, request)
     return ws
+
+
+async def process_connection(ws, request):
+    user_id = await authorize_new_user(request)
+    logging.info("websocket connection opened with user_id {}".format(user_id))
+    try:
+        register_new_user(user_id)
+    except NotPermittedUserException as e:
+        await exit_connection(ws, user_id, e)
+    else:
+        register_socket(ws, user_id)
+        await process_messages(ws, user_id)
+
+
+async def authorize_new_user(request):
+    return await authorized_userid(request)
+
+
+def register_socket(ws, user_id):
+    registry.add_socket(user_id, ws)
+
+
+def register_new_user(user_id):
+    try:
+        global_playground.register(user_id)
+    except AlreadyRegistered:
+        raise NotPermittedUserException("User already registered")
+
+
+async def exit_connection(ws, user_id, ex):
+    error_message = "Deliberately closed connection with user_id {}! Reason: ".format(user_id) + ex.msg
+    logging.info(error_message)
+    await send_command(ErrorCommand(user_id, msg=error_message))
+    await ws.close()
+
+
+async def process_messages(ws, user_id):
+    async for msg in ws:
+        if msg.type == WSMsgType.TEXT:
+            await handle_command(json.loads(msg.data), user_id)
+        elif msg.type == WSMsgType.ERROR:
+            logging.info('connection closed with exception {} with user_id {}'.format(ws.exception(), user_id))
+            await handle_error(user_id)
+        elif msg.type == WSMsgType.BINARY:
+            logging.info('Received BINARY type message')
+        elif msg.type == WSMsgType.CLOSE:
+            logging.info('Received CLOSE type message')
+        elif msg.type == WSMsgType.CLOSED:
+            logging.info('Received CLOSED type message')
+        elif msg.type == WSMsgType.CLOSING:
+            logging.info('Received CLOSING type message')
+        elif msg.type == WSMsgType.CONTINUATION:
+            logging.info('Received CONTINUATION type message')
+        elif msg.type == WSMsgType.PING:
+            logging.info('Received PING type message')
+        elif msg.type == WSMsgType.PONG:
+            logging.info('Received PONG type message')
+
+    logging.info('websocket connection closed with user_id {}'.format(user_id))
+    await handle_error(user_id)
+
+
+class NotPermittedUserException(Exception):
+    def __init__(self, msg):
+        super().__init__()
+        self.msg = msg
